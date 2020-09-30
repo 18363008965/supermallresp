7035a34974ff97acdf84f7d17dc3f00563dcbb72
together <2629192232@qq.com>
2020-09-30 17:29:02 +0800

    '详情页中商品信息的展示开发'

--- src/network/detail.js
+++ src/network/detail.js
@@ -11,3 +11,19 @@ export function getDetail(iid) {
     }
   })
 }
+
+/*
+* 将详情页中的数据封装到一个类中
+* */
+export class Goods {
+  constructor(itemInfo, columns, services) {
+    this.title = itemInfo.title
+    this.desc = itemInfo.desc
+    this.newPrice = itemInfo.price
+    this.oldPrice = itemInfo.oldPrice
+    this.discount = itemInfo.discountDesc
+    this.columns = columns
+    this.services = services
+    this.realPrice = itemInfo.lowNowPrice
+  }
+}
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,35 +2,42 @@
   <div id="detail">
     <detail-nav-bar></detail-nav-bar>
     <detail-swiper :top-images="topImages"/>
+    <detail-base-info :goods="goods"></detail-base-info>
   </div>
 </template>
 
 <script>
   import DetailNavBar from "./childComps/DetailNavBar";
   import DetailSwiper from "./childComps/DetailSwiper";
-  import {getDetail} from "../../network/detail";
+  import DetailBaseInfo from "./childComps/DetailBaseInfo";
+  import {getDetail, Goods} from "../../network/detail";
 
   export default {
     name: "Detail",
     data() {
       return {
         iid: null,
-        topImages: []
+        topImages: [],
+        goods: {}
       }
     },
     components: {
       DetailNavBar,
-      DetailSwiper
+      DetailSwiper,
+      DetailBaseInfo
     },
     created() {
       // 1.保存传入的iid
       this.iid = this.$route.params.iid
-      //1.请求轮播图中要显示的图片数据
+      //2.根据iid获取详情数据
       getDetail(this.iid).then(res => {
-        //打印请求过来的数据
+        //1.请求轮播图中要显示的图片数据
         console.log(res);
         const data = res.result;
         this.topImages = data.itemInfo.topImages;
+
+        //2.获取商品信息
+        this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services);
       });
     }
   }
