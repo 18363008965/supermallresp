c5922172a32c0be613b1679462ebc71b86153903
together <2629192232@qq.com>
2020-10-15 16:43:54 +0800

    ',,,,'

--- src/network/request.js
+++ src/network/request.js
@@ -4,9 +4,9 @@
 import axios from 'axios'
 
 export function request(config) {
-  // 1.创建axios的实例
+
   const instance = axios.create({
-    baseURL: 'xxx',
+    baseURL: '',
     timeout: 5000
   })
 
