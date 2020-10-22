f3f11a007bb08bbd51dfaa425a48d779e7aca728
together <2629192232@qq.com>
2020-10-22 09:58:24 +0800

    '购物车页面底部全选去计算组件的开发以及个人主页部分组件代码复制'

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -2,12 +2,14 @@
    <div class="cart">
      <nav-bar class="nav-bar"><div slot="center">购物车({{cartLength}})</div></nav-bar>
      <cart-list :cart-list="cartList"/>
+     <cart-bottom-bar/>
    </div>
 </template>
 
 <script>
   import NavBar from "../../components/common/navbar/NavBar";
   import CartList from "./childComps/CartList";
+  import CartBottomBar from "./childComps/CartBottomBar";
 
   import {mapGetters} from 'vuex'
 
@@ -15,7 +17,8 @@
     name: "Cart",
     components: {
       NavBar,
-      CartList
+      CartList,
+      CartBottomBar
     },
     computed: {
       ...mapGetters([
